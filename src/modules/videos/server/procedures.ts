ac46193a71735debe6a59b591d8b48725ae19360
Anthony David <anthony.david34@sfr.fr>
2025-02-13 16:13:28 +0100

    uploadthing clean up

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -4,6 +4,7 @@ import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { and, eq } from "drizzle-orm";
+import { UTApi } from "uploadthing/server";
 import { z } from "zod";
 
 export const videosRouter = createTRPCRouter({
@@ -97,6 +98,17 @@ export const videosRouter = createTRPCRouter({
 
       if (!existingVideo) {
         throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      /* Clean uploathing old thumbnail */
+      if (existingVideo.thumbnailKey) {
+        const utapi = new UTApi();
+
+        await utapi.deleteFiles(existingVideo.thumbnailKey);
+        await db
+          .update(videos)
+          .set({ thumbnailKey: null, thumbnailUrl: null })
+          .where(and(eq(videos.id, input.id), eq(videos.userId, userId)));
       }
 
       if (!existingVideo.muxPlaybackId) {
