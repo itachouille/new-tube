a9378c89e79ad53a6f2eea123c003122be7c66db
Anthony David <anthony.david34@sfr.fr>
2025-02-19 10:24:38 +0100

    video view

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
+import { VideoBanner } from "../components/video-banner";
+import { VideoTopRow } from "../components/video-top-row";
 
 interface VideoSectionProps {
   videoId: string;
@@ -22,8 +26,24 @@ const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
   const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
 
   return (
-    <div>
-      <p>{JSON.stringify(video)}</p>
-    </div>
+    <>
+      <div
+        className={cn(
+          "aspect-video bg-black rounded-xl overflow-hidden relative",
+          video.muxStatus !== "ready" && "rounded-b-none"
+        )}
+      >
+        <VideoPlayer
+          autoPlay
+          onPlay={() => {}}
+          playbackId={video.muxPlaybackId}
+          thumbnailUrl={video.thumbnailUrl}
+        />
+      </div>
+
+      <VideoBanner status={video.muxStatus} />
+
+      <VideoTopRow video={video} />
+    </>
   );
 };
